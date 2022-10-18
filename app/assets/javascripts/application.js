831c916025730624f7f63f89014773373d270d1f
dth2k1 <gblhiep@gmail.com>
2022-10-18 08:41:53 +0700

    refs #2 auth devise importjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,1 @@
-let modal = document.getElementById("myModal");
-function closeAlert(duration) {
-  setTimeout(function () {
-    modal.parentNode.removeChild(modal);
-  }, duration);
-}
-
-window.onload = function () {
-  closeAlert(2000);
-  modal.style.display = "block";
-};
+//= require_tree .
