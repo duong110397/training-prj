2cfce7aa709d70ba60bbafe39e07cc57993e5498
dth2k1 <gblhiep@gmail.com>
2022-10-19 08:45:05 +0700

    user manager

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #delete devise validate to custome validate
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+  validates :name, presence: true, length: {maximum: 50, message: "name is so long, please choose short name"} 
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX, message: "email is invalid"}, uniqueness: {case_sensetive:true}
+  validates :password, presence: true
 end
