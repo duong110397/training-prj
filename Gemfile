3950876856cf7b09d156db691b6c5e5e93114fee
dth2k1 <gblhiep@gmail.com>
2022-10-27 23:29:45 +0700

    refs #11 google login

--- Gemfile
+++ Gemfile
@@ -43,7 +43,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem 'bootstrap-sass', '3.4.1'
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
-
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem "omniauth-rails_csrf_protection"
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #custome router devise
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" },controllers: {
-    sessions: 'users/sessions'
+    omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions'
   }
   resources :users
   root to: "main#index"
