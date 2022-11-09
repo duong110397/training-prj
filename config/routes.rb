1a5c02a5e317418fef1fa4035b7607c9e3ad53f2
dth2k1 <gblhiep@gmail.com>
2022-11-09 14:05:32 +0700

    refs #21 fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   #custome router devise
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" },controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions'
-  }
+  }, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   resource :posts do
     collection do
       get :all_posts_guest
